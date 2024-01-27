eca8c8c0a2479e05892f32da4a5c606e65d1b54e
Hasnathasan <hasnatoooooooo@gmail.com>
2024-01-28 01:21:12 +0600

    Some code changed in myorder and update.jsx file

--- src/layOut/Pages/MyPage/MyOrder.jsx
+++ src/layOut/Pages/MyPage/MyOrder.jsx
@@ -5,96 +5,119 @@ import { Helmet } from "react-helmet";
 import { useLoaderData } from "react-router-dom";
 import Swal from "sweetalert2";
 
-
 const MyOrder = () => {
-    const loadedMyOrder = useLoaderData();
-    const [updateUser, setUpdateUser] = useState(loadedMyOrder);
-    console.log(updateUser)
-    const {user, loading} = useContext(AuthContext);
-console.log(user?.email)
+  const loadedMyOrder = useLoaderData();
+  const [updateUser, setUpdateUser] = useState(loadedMyOrder);
+  console.log(updateUser);
+  const { user, loading } = useContext(AuthContext);
+  console.log(user?.email);
 
+  useEffect(() => {
+    if (loadedMyOrder && user?.email) {
+      const filterCard = loadedMyOrder.filter(
+        (item) => item.email === user.email
+      );
+      setUpdateUser(filterCard);
+    }
+  }, [loadedMyOrder, user?.email]);
 
-useEffect(() => {
-  if (loadedMyOrder && user?.email) {
-    const filterCard = loadedMyOrder.filter((item) => item.email === user.email);
-    setUpdateUser(filterCard);
-  }
-}, [loadedMyOrder, user?.email]);
-
-   const handleDelete = (id) => {
-console.log(id);
-Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!"
-  }).then((result) => {
-    console.log(result)
-    if (result.isConfirmed) {
-        fetch(`https://bakery-server-psi.vercel.app/order/${id}`,{
-            method: 'DELETE'
+  const handleDelete = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      console.log(result);
+      if (result.isConfirmed) {
+        fetch(`https://bakery-server-psi.vercel.app/order/${id}`, {
+          method: "DELETE",
         })
-        .then(res => res.json())
-        .then(data => {
-           if(data.deletedCount > 0){
-            Swal.fire({
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
                 title: "Deleted!",
                 text: "Your file has been deleted.",
-                icon: "success"
+                icon: "success",
               });
               const remaining = updateUser.filter((item) => item._id !== id);
-              setUpdateUser(remaining)
-           }
-        })
+              setUpdateUser(remaining);
+            }
+          });
+      }
+    });
+  };
 
-   
-    }
-  });
-   };
-
-  if(loading) return <div className="flex justify-center my-10"><Spinner className="h-8 w-8" /></div> ;
-
+  if (loading)
     return (
-        <div className="container mx-auto">
-              <Helmet>
-                <meta charSet="utf-8" />
-                <title>SL Bakery | Client order </title>
-                <link rel="canonical" href="http://mysite.com/example" />
-            </Helmet>
-            
-            <div className="">
-                <h1 className="text-2xl md:text-4xl text-center my-10">{user?.displayName}s Ordered Items</h1>
-                <div className="grid grid-cols-1 gap-5 pl-4 md:pl-40 lg:pl-0 lg:grid-cols-3">
-                    {
-                       updateUser.length ? updateUser.map((user) => (
-                        <div  data-aos="flip-left"
-                        data-aos-easing="ease-out-cubic"
-                        data-aos-duration="2000" key={user?._id} className="card w-96 bg-base-100 shadow-xl image-full">
-                        <figure><img src={user?.image} alt="Shoes" /></figure>
-                        <div className="card-body">
-                          <h2 className="card-title">{user?.foodname}</h2>
-                          
-                          <p className="mb-10">{user?.customerName}</p>
-                          <p>Price: $ {user?.Price}</p>
-                          <p>Date: {user?.Date}</p>
-                          <div className="card-actions justify-end">
-                            <button onClick={() => handleDelete(user?._id)} className="btn btn-primary">delete</button>
-                          </div>
-                        </div>
-                      </div>
-                       )) 
-                       : <>
-                       <div></div>
-                       <div className="lg:text-center lg:pl-0">Must be <span className="text-bold text-green-600 text-xl">Hungry??</span>..Why don't you order a food first......</div>
-                       </> 
-                    }
-                </div>
-            </div>
-        </div>
+      <div className="flex justify-center my-10">
+        <Spinner className="h-8 w-8" />
+      </div>
     );
+
+  return (
+    <div className="container mx-auto">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>SL Bakery | Client order </title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+
+      <div className="">
+        <h1 className="text-2xl md:text-4xl text-center my-10">
+          {user?.displayName}s Ordered Items
+        </h1>
+        <div className="grid grid-cols-1 gap-5 pl-4 md:pl-40 lg:pl-0 lg:grid-cols-3">
+          {updateUser.length ? (
+            updateUser.map((user) => (
+              <div
+                data-aos="flip-left"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="2000"
+                key={user?._id}
+                className="card w-96 bg-base-100 shadow-xl image-full"
+              >
+                <figure>
+                  <img src={user?.image} alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">{user?.foodname}</h2>
+
+                  <p className="mb-10">{user?.customerName}</p>
+                  <p>Price: $ {user?.Price}</p>
+                  <p>Date: {user?.Date}</p>
+                  <div className="card-actions justify-end">
+                    <button
+                      onClick={() => handleDelete(user?._id)}
+                      className="btn btn-primary"
+                    >
+                      delete
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <>
+              <div></div>
+              <div className="lg:text-center lg:pl-0">
+                Must be{" "}
+                <span className="text-bold text-green-600 text-xl">
+                  Hungry??
+                </span>
+                ..Why don't you order a food first......
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyOrder;
--- src/layOut/Pages/MyPage/Update.jsx
+++ src/layOut/Pages/MyPage/Update.jsx
@@ -6,122 +6,194 @@ import { useLoaderData } from "react-router-dom";
 import Swal from "sweetalert2";
 import { motion } from "framer-motion";
 
-
 const Update = () => {
-    const loadItem = useLoaderData();
-    const {foodname, _id, recipie, img, Price, origin, quantity} = loadItem ;
-    console.log(loadItem)
-    const {user, loading} = useContext(AuthContext);
-    if(loading) return <div className="flex justify-center my-10"><Spinner className="h-8 w-8" /></div> ;
+  const loadItem = useLoaderData();
+  const { foodname, _id, recipie, img, Price, origin, quantity } = loadItem;
+  console.log(loadItem);
+  const { user, loading } = useContext(AuthContext);
+  if (loading)
+    return (
+      <div className="flex justify-center my-10">
+        <Spinner className="h-8 w-8" />
+      </div>
+    );
 
-    const handleUpdate = e => {
-      e.preventDefault();
+  const handleUpdate = (e) => {
+    e.preventDefault();
 
-      const foodName = e.target.foodName.value;
-      const pic = e.target.pic.value;
-      const quantity = e.target.quantity.value;
-      const price= e.target.price.value;
-      const name = e.target.name.value;
-      const email = e.target.email.value;
-      const origin = e.target.origin.value;
-      const recipie = e.target.recipie.value;
+    const foodName = e.target.foodName.value;
+    const pic = e.target.pic.value;
+    const quantity = e.target.quantity.value;
+    const price = e.target.price.value;
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const origin = e.target.origin.value;
+    const recipie = e.target.recipie.value;
 
-      const updateditem = {
-        foodName, pic, quantity, price, name, email, origin, recipie
-      };
-     
-      fetch(`https://bakery-server-psi.vercel.app/item/${_id}`, {
-        method: 'PUT',
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify(updateditem)
-      })
-      .then(res => res.json())
-      .then(data => {
-        if(data.matchedCount) {
-          Swal.fire({
-              title: 'success!!',
-              text: 'Item Details Updated Successfully',
-              icon: 'success',
-              confirmButtonText: 'Cool'
-            })
-      }
-      })
+    const updateditem = {
+      foodName,
+      pic,
+      quantity,
+      price,
+      name,
+      email,
+      origin,
+      recipie,
     };
 
-    return (
-        <div className="container mx-auto">
-                <Helmet>
-                <meta charSet="utf-8" />
-                <title>SL Bakery | Client update page </title>
-                <link rel="canonical" href="http://mysite.com/example" />
-            </Helmet>
-            <div className="text-center mt-12 mb-5 text-3xl font-semibold">Update Your Items Info</div>
-            <div className="hero-content">    
-    <div className=" bg-base-100 shadow-2xl">
-      <div></div>
-      <form onSubmit={handleUpdate} className="card-body  my-8">      
-     <div data-aos="zoom-in-up" data-aos-duration="2000" className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-<div className="form-control">
-    <label className="label">
-      <span className="label-text">Food Name</span>
-    </label>
-    <input type="text" name="foodName" defaultValue={foodname} placeholder="Type here" className="input input-bordered input-info w-full max-w-lg"  />
-  </div>
-<div className="form-control">
-    <label className="label">
-      <span className="label-text">Food Image</span>
-    </label>
-    <input type="text" name="pic" placeholder="Type here" defaultValue={img} className="input input-bordered input-info w-full max-w-lg"/>
-  </div>
-  <div className="form-control">
-    <label className="label">
-      <span className="label-text">Price</span>
-    </label>
-    <input type="text"  name="price" placeholder="Type here" defaultValue={Price} className="input input-bordered input-info w-full max-w-lg" />
-  </div>
-  <div className="form-control">
-    <label className="label">
-      <span className="label-text">Quantity</span>
-    </label>
-    <input type="text" name="quantity" defaultValue={quantity} placeholder="Type here" className="input input-bordered input-info w-full max-w-lg" />
-  </div>
-  <div className="form-control">
-    <label className="label">
-      <span className="label-text">Buyer Name</span>
-    </label>
-    <input type="text" defaultValue={user?.displayName}  name="name" placeholder="Type here" className="input input-bordered input-info w-full max-w-lg" required readOnly={true}/>
-  </div>
-  <div className="form-control">
-    <label className="label">
-      <span className="label-text">Buyer Email</span>
-    </label>
-    <input type="Email" defaultValue={user?.email} name="email" placeholder="Type here" className="input input-bordered input-info w-full max-w-lg" required readOnly={true}/>
-  </div>
-  <div className="form-control">
-    <label className="label">
-      <span className="label-text">Origin</span>
-    </label>
-    <input type="text" name="origin" defaultValue={origin} placeholder="Type here" className="input input-bordered input-info w-full max-w-lg"  />  
-  </div>
-  <div className="form-control">
-    <label className="label">
-      <span className="label-text">Recipie</span>
-    </label>
-    <textarea name="recipie" defaultValue={recipie} className="textarea textarea-info max-w-lg" placeholder="Bio"></textarea>
-   
-  </div>
-  </div>
-  <div className="form-control mt-6">
-    <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className="btn btn-active btn-accent ">Update Item</motion.button>
-  </div>
- 
-</form>
-    </div>
-  </div>
+    fetch(`https://bakery-server-psi.vercel.app/item/${_id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updateditem),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.matchedCount) {
+          Swal.fire({
+            title: "success!!",
+            text: "Item Details Updated Successfully",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
+  };
+
+  return (
+    <div className="container mx-auto">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>SL Bakery | Client update page </title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <div className="text-center mt-12 mb-5 text-3xl font-semibold">
+        Update Your Items Info
+      </div>
+      <div className="hero-content">
+        <div className=" bg-base-100 shadow-2xl">
+          <div></div>
+          <form onSubmit={handleUpdate} className="card-body  my-8">
+            <div
+              data-aos="zoom-in-up"
+              data-aos-duration="2000"
+              className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3"
+            >
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Food Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="foodName"
+                  defaultValue={foodname}
+                  placeholder="Type here"
+                  className="input input-bordered input-info w-full max-w-lg"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Food Image</span>
+                </label>
+                <input
+                  type="text"
+                  name="pic"
+                  placeholder="Type here"
+                  defaultValue={img}
+                  className="input input-bordered input-info w-full max-w-lg"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Price</span>
+                </label>
+                <input
+                  type="text"
+                  name="price"
+                  placeholder="Type here"
+                  defaultValue={Price}
+                  className="input input-bordered input-info w-full max-w-lg"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Quantity</span>
+                </label>
+                <input
+                  type="text"
+                  name="quantity"
+                  defaultValue={quantity}
+                  placeholder="Type here"
+                  className="input input-bordered input-info w-full max-w-lg"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Buyer Name</span>
+                </label>
+                <input
+                  type="text"
+                  defaultValue={user?.displayName}
+                  name="name"
+                  placeholder="Type here"
+                  className="input input-bordered input-info w-full max-w-lg"
+                  required
+                  readOnly={true}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Buyer Email</span>
+                </label>
+                <input
+                  type="Email"
+                  defaultValue={user?.email}
+                  name="email"
+                  placeholder="Type here"
+                  className="input input-bordered input-info w-full max-w-lg"
+                  required
+                  readOnly={true}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Origin</span>
+                </label>
+                <input
+                  type="text"
+                  name="origin"
+                  defaultValue={origin}
+                  placeholder="Type here"
+                  className="input input-bordered input-info w-full max-w-lg"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Recipie</span>
+                </label>
+                <textarea
+                  name="recipie"
+                  defaultValue={recipie}
+                  className="textarea textarea-info max-w-lg"
+                  placeholder="Bio"
+                ></textarea>
+              </div>
+            </div>
+            <div className="form-control mt-6">
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="btn btn-active btn-accent "
+              >
+                Update Item
+              </motion.button>
+            </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Update;
