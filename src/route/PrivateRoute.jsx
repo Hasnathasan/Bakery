d4a17cc567dbfa220dc919454885e9c0bb7517d5
Hasnathasan <hasnatoooooooo@gmail.com>
2024-01-29 02:00:45 +0600

    Private route added

--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -3,17 +3,21 @@ import { AuthContext } from "./AuthProvider";
 import { Spinner } from "@material-tailwind/react";
 import { Navigate, useLocation } from "react-router-dom";
 
-
 const PrivateRoute = ({ children }) => {
-    const { user, loading } = useContext(AuthContext);
-    const location = useLocation();
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
 
-    if(loading) return <div className="flex justify-center my-10"><Spinner className="h-8 w-8" /></div> ;
+  if (loading)
+    return (
+      <div className="flex justify-center my-10">
+        <Spinner className="h-8 w-8" />
+      </div>
+    );
 
-    if(!user?.email) {
-        return <Navigate state={location.pathname} to="/login"></Navigate>
-    }
-    return children;
+  if (!user?.email) {
+    return <Navigate state={location.pathname} to="/login"></Navigate>;
+  }
+  return children;
 };
 
 export default PrivateRoute;
