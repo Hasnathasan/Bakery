ec8b2049c08f3de13cdca49a39c2d49dca9f6f92
Hasnathasan <hasnatoooooooo@gmail.com>
2024-01-29 02:13:04 +0600

    Changes in the MainLayOut.jsx file

--- src/layOut/Pages/MainLayOut.jsx
+++ src/layOut/Pages/MainLayOut.jsx
@@ -2,19 +2,18 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../../components/header/Navbar";
 import Footer from "../../components/Footer";
 
-
 const MainLayOut = () => {
-    return (
-        <div className="flex flex-col min-h-[100vh]">
-         <div className="flex-grow">
-         <Navbar></Navbar>
-            <Outlet></Outlet>
-         </div>
-            <div className="flex-shrink-0">
-            <Footer></Footer>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-[100vh]">
+      <div className="flex-grow">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+      <div className="flex-shrink-0">
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayOut;
